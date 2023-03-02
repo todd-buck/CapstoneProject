310b92ebef0a9736a47ec72ad8af10c4fa83791d
mclewis8 <mclewis8@crimson.ua.edu>
2023-03-02 14:31:18 -0600

    Pushing branch to GitHub

--- DDRInventory-UI/src/scenes/dashboard/addnewproduct.jsx
+++ DDRInventory-UI/src/scenes/dashboard/addnewproduct.jsx
@@ -1,8 +1,72 @@
+import React from 'react';
+import {
+    Typography,
+    FormControl,
+    InputLabel,
+    MenuItem,
+    Select,
+    TextField,
+    InputAdornment,
+} from '@mui/material';
+import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
+
+import { useTheme } from "@mui/material";
+import { tokens } from "../../theme";
+
 const AddNewProductComponent = () => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
 
     return (
-        <p>Welcome to the AddNewItem page!</p>
+        <div>
+            <Typography variant="h1" sx={{ p: 2 }}>
+                Add New Product
+            </Typography>
+            <FormControl>
+                <TextField
+                    id="name"
+                    label="Name"
+                    variant="outlined"
+                    sx={{mb:2, width:'300px'}}
+                />
+                <TextField
+                    id="quantity"
+                    label="Quantity"
+                    type="number"
+                    inputMode="decimal"
+                    step="0.01"
+                    variant="outlined"
+                    sx={{ mb: 2, width: '300px' }}
+                />
+                <TextField
+                    id="price"
+                    label="Price"
+                    type="number"
+                    inputMode="decimal"
+                    step="0.01"
+                    variant="outlined"
+                    sx={{width: '150px' }}
+                    InputProps={{
+                        startAdornment: (
+                            <InputAdornment position="start">
+                                <AttachMoneyIcon/>
+                            </InputAdornment>
+                        ),
+                    }}
+                />
+                <Select
+                    id="select-option"
+                    variant="outlined"
+                >
+                    <MenuItem value="option1">Option 1</MenuItem>
+                    <MenuItem value="option2">Option 2</MenuItem>
+                    <MenuItem value="option3">Option 3</MenuItem>
+                    <MenuItem value="other">Other</MenuItem>
+                </Select>
+            </FormControl>
+        </div>
     );
-}
+};
 
 export default AddNewProductComponent;
+
--- DDRInventory-UI/src/scenes/dashboard/yourprofile.jsx
+++ DDRInventory-UI/src/scenes/dashboard/yourprofile.jsx
@@ -1,8 +1,84 @@
+import React from 'react';
+import { Avatar, Typography, TextField, Button, Box } from '@mui/material';
+import profilePicture from './StockProfilePicture.jpg';
+
+import { useTheme } from "@mui/material";
+import { tokens } from "../../theme";
+
 const YourProfileComponent = () => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    const emailAddress = "margarethamilton@dillardsdiningresources.com";
+    const phoneNumber = "1800-867-5309";
+    const loggedin = false;
+
+    if (loggedin) return (
+        <div>
+            <Typography variant="h1" sx={{p:2}}>
+                Your Profile
+            </Typography>
+            <Box sx={{ display: 'flex', alignItems: 'center', bgcolor: colors.primary[400], p: 3}}>
+                <Avatar src={profilePicture} alt="Profile Picture" sx={{
+                    width: '200px',
+                    height: '200px',
+                    border: `5px solid ${colors.primary[100]}`
+                }} />
+                <Box sx={{ ml: 2 }}>
+                    <Typography variant="h1" sx={{ mb: 1 }}>
+                        Margaret Hamilton
+                    </Typography>
+                    <Typography variant="h3" sx={{ mb: 1 }}>
+                        Software Engineer
+                    </Typography>
+                    <Typography variant="p" sx={{ mb: 1 }}>
+                        <div>
+                            <b>Email: </b>{emailAddress}
+                        </div>
+                        <div>
+                            <b>Phone: </b>{phoneNumber}
+                        </div>
+                    </Typography>
+                </Box>
+            </Box>
+            <Button variant="contained" sx={{
+                ml: 2, mt: 2,
+                backgroundColor: colors.blueAccent[400],
+                ':hover': { backgroundColor: colors.blueAccent[600] }
+            }}>
+                Edit Profile
+            </Button>
+            <Button variant="contained" sx={{
+                ml: 2, mt: 2,
+                backgroundColor: colors.greenAccent[400],
+                ':hover': { backgroundColor: colors.greenAccent[600] }
+            }}>
+                Log Out
+            </Button>
+        </div>
+    );
 
     return (
-        <p>Welcome to the YourProfile page!</p>
+        <div>
+            <Typography variant="h1" sx={{ p: 2 }}>
+                Account Login
+            </Typography>
+            <Box sx={{ maxWidth:'450px', display: 'flex', flexDirection: 'column', gap: '1rem' }}>
+                <TextField
+                    label="Username"
+                />
+                <TextField
+                    label="Password"
+                    type="password"
+                />
+                <Button variant="contained" sx={{
+                    backgroundColor: colors.greenAccent[400],
+                    ':hover': { backgroundColor: colors.greenAccent[600] }
+                }}>
+                    Login
+                </Button>
+            </Box>
+        </div>
     );
-}
- 
+};
+
 export default YourProfileComponent;

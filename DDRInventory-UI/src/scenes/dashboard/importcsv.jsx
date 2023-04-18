6432f68aa0e23fc07de65f4d13195f5130f4933f
mclewis8 <mclewis8@crimson.ua.edu>
2023-04-18 17:49:28 -0500

    ImportCSV redesign
    Changes:
    - modifications only to importcsv.jsx
    - replaced return elements with mui elements

--- DDRInventory-UI/src/scenes/dashboard/importcsv.jsx
+++ DDRInventory-UI/src/scenes/dashboard/importcsv.jsx
@@ -1,17 +1,29 @@
 import { useState } from 'react';
 import UnderConstructionComponent from '../global/underConstruction.jsx'
+import { Box, Button, FormControl, Typography } from '@mui/material';
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
+import InsertDriveFileIcon from "@mui/icons-material/InsertDriveFile";
+
+import useTheme from "@mui/material/styles/useTheme";
+import { tokens } from "../../theme";
 
 import { target_URL } from "../../App.js"
-
 const isFinished = true;
 
 
 const ImportCSVComponent = () => {
+
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode, theme.palette.scheme);
+
     // eslint-disable-next-line
     const [file, setFile] = useState()
+    const [fileSelected, setFileSelected] = useState(false);
+
 
     function handleChange(event) {
         setFile(event.target.files[0])
+        setFileSelected(true);
     }
 
     function handleSubmit(event) {
@@ -35,15 +47,59 @@ const ImportCSVComponent = () => {
     }
 
     if (isFinished) {
+
         return (
-            <div className="ImportCSVComponent">
-                <form onSubmit={handleSubmit}>
-                    <h1>CSV File Upload</h1>
-                    <input type="file" accept=".csv" onChange={handleChange} />
-                    <button type="submit" onclick="handleSubmit(file)">Upload</button>
-                </form>
-            </div>
+            <Box
+                sx={{
+                    margin: '20px',
+                }}
+            >
+                <Typography variant="h1" sx={{ marginBottom: '20px' }}>
+                    Upload CSV
+                </Typography>
+                <FormControl onSubmit={handleSubmit}>
+                    <Button
+                        variant="contained"
+                        component="label"
+                        startIcon={fileSelected ?
+                            <InsertDriveFileIcon />
+                            : < CloudUploadIcon />
+                        }
+                        sx={{
+                            width: '300px',
+                            height: '200px',
+                            color: colors.gray[100],
+                            fontSize: '20px',
+                            backgroundColor: colors.primary[200],
+                            '&:hover': {
+                                backgroundColor: colors.primary[300],
+                            },
+                        }}
+                    >
+                        {fileSelected ? file.name : 'Choose file'}
+                        <input
+                            id="file-input"
+                            type="file"
+                            accept=".csv"
+                            onChange={handleChange}
+                            style={{ display: 'none' }}
+                        />
+                    </Button>
+                    <Button type="submit" sx={{
+                        marginTop: '10px',
+                        color: colors.gray[100],
+                        backgroundColor: colors.addAccent[300],
+                        '&:hover': {
+                            backgroundColor: colors.addAccent[500],
+                        },
+                    }} color="success" onclick="handleSubmit(file)">
+                        Upload
+                    </Button>
+                </FormControl>
+            </Box>
         );
+
+
     } else {
         return (
             <UnderConstructionComponent />

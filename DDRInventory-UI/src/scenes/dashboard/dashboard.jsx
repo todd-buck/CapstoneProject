34dd60cb5492f88dabfe4d4f30920e8c7fcea518
Jared Patterson <mjpatterson4@crimson.ua.edu>
2023-04-19 17:28:17 -0500

    Dashboard Component: Pie Chart -displays all top items with the most putaway storage taken up that are of the subCategory "Meat"

--- DDRInventory-UI/src/scenes/dashboard/dashboard.jsx
+++ DDRInventory-UI/src/scenes/dashboard/dashboard.jsx
@@ -1,10 +1,71 @@
-import UnderConstructionComponent from '../global/underConstruction.jsx'
+import React, { useState, useEffect } from 'react';
+import ReactEcharts from 'echarts-for-react';
+import { target_URL } from "../../App.js";
+import {
+    useQuery
+} from '@tanstack/react-query';
 
-const DashboardComponent = () => {
+const PieChart = () => {
 
-    return (
-        <UnderConstructionComponent />
-    );
-} 
+    const { data, isFetchError, isFetching, refetch } = useQuery({
+        queryKey: [
+            'table-data',
+        ],
+        queryFn: async () => {
+            const fetchURL = new URL(
+                '/api/item/catalog',
+                target_URL,
+            );
 
-export default DashboardComponent;
+            const response = await fetch(fetchURL.href);
+            const json = await response.json();
+
+            const chartData = json
+                .filter(item => item.subCategory === "Meat")
+                .sort((a, b) => b.quantityOnHand - a.quantityOnHand)
+                .map(({ name, quantityOnHand }) => ({
+                    name,
+                    value: quantityOnHand,
+                }));
+
+            return chartData;
+        }
+    });
+
+
+    const options = {
+        title: {
+            text: 'Meat Items On Hand',
+            left: 'center',
+        },
+        tooltip: {
+            trigger: 'item',
+        },
+        legend: {
+            orient: 'vertical',
+            left: 'left',
+        },
+        series: [
+            {
+                name: 'Quantity on Hand',
+                type: 'pie',
+                radius: '50%',
+                data: data,
+                emphasis: {
+                    itemStyle: {
+                        shadowBlur: 0,
+                        shadowOffsetX: 0,
+                        shadowColor: 'rgba(0, 0, 0, 0.5)',
+                    },
+                },
+                label: {
+                    formatter: '{b}: {c} ({d}%)',
+                },
+            },
+        ],
+    };
+
+    return <ReactEcharts option={options} />;
+};
+
+export default PieChart;

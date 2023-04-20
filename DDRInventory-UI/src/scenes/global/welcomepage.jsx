c76f9a11d595c056517c2ad53e88261233fb0d91
Wade Pelham <wapelham@crimson.ua.edu>
2023-04-20 15:47:08 -0500

    Welcome Page Popup Update 1 I realized that my original implementation of the welcome page was not the most intelligent solution.
    Changes
    - Changed the welcome page popup to be a modal.
    - Made it to where the popup is not tied to a button.
    - Made it to where the popup loads on webpage startup.
    
    Notes
    - Still not mobile friendly... yet
    
    Next Steps/Action Items
    - Add a background to the popup.  (Probably with the use of z-indexes and layers)
    - Make the popup mobile friendly

--- DDRInventory-UI/src/scenes/global/welcomepage.jsx
+++ DDRInventory-UI/src/scenes/global/welcomepage.jsx
@@ -1,115 +1,79 @@
 import * as React from 'react';
-import PropTypes from 'prop-types';
+import Backdrop from '@mui/material/Backdrop';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Fade from '@mui/material/Fade';
 import Button from '@mui/material/Button';
-import { styled } from '@mui/material/styles';
-import Dialog from '@mui/material/Dialog';
-import DialogTitle from '@mui/material/DialogTitle';
-import DialogContent from '@mui/material/DialogContent';
-import DialogActions from '@mui/material/DialogActions';
-import IconButton from '@mui/material/IconButton';
-import CloseIcon from '@mui/icons-material/Close';
 import Typography from '@mui/material/Typography';
 
-import Box from "@mui/material/Box";
+import Link from '@mui/material/Link';
 import logo from '../../assets/BreezeLogo.png';
 
-const BootstrapDialog = styled(Dialog)(({ theme }) => ({
-    '& .MuiDialogContent-root': {
-        padding: theme.spacing(2),
-    },
-    '& .MuiDialogActions-root': {
-        padding: theme.spacing(1),
-    },
-}));
-
-function BootstrapDialogTitle(props) {
-    const { children, onClose, ...other } = props;
-
-    return (
-        <DialogTitle sx={{ m: 0, p: 2 }} {...other}>
-            {children}
-            {onClose ? (
-                <IconButton
-                    aria-label="close"
-                    onClick={onClose}
-                    sx={{
-                        position: 'absolute',
-                        right: 8,
-                        top: 8,
-                        color: (theme) => theme.palette.grey[500],
-                    }}
-                >
-                    <CloseIcon />
-                </IconButton>
-            ) : null}
-        </DialogTitle>
-    );
-}
-
-BootstrapDialogTitle.propTypes = {
-    children: PropTypes.node,
-    onClose: PropTypes.func.isRequired,
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 500,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 2,
 };
 
-export default function CustomizedDialogs() {
-    const [open, setOpen] = React.useState(false);
-
-    const handleClickOpen = () => {
-        setOpen(true);
-    };
-    const handleClose = () => {
-        setOpen(false);
-    };
+export default function TransitionsModal() {
+    const [open, setOpen] = React.useState(true);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
 
     return (
         <div>
-            <Button variant="outlined" onClick={handleClickOpen}>
-                Open dialog popup
-            </Button>
-            <BootstrapDialog
-                onClose={handleClose}
-                aria-labelledby="customized-dialog-title"
+            <Modal
+                aria-labelledby="transition-modal-title"
+                aria-describedby="transition-modal-description"
                 open={open}
+                onClose={handleClose}
+                closeAfterTransition
+                slots={{ backdrop: Backdrop }}
+                slotProps={{
+                    backdrop: {
+                        timeout: 500,
+                    },
+                }}
             >
-                
-                <Box mt="25px">
-                    <Box display="flex" justifyContent="center" alignItems="center">
-                        <img
-                            alt="profile-user"
-                            width="100px"
-                            height="100px"
-                            src={logo}
-                            style={{ cursor: "pointer", borderRadius: "50%" }}
-                        />
+                <Fade in={open}>
+                    <Box align="center" sx={style}>
+                        <Box mt="25px">
+                            <Box display="flex" justifyContent="center" alignItems="center">
+                                <img
+                                    alt="profile-user"
+                                    width="100px"
+                                    height="100px"
+                                    src={logo}
+                                    style={{ cursor: "pointer", borderRadius: "50%" }}
+                                />
+                            </Box>
+                        </Box>
+                        <Box
+                            sx={{
+                                color: 'black',
+                                fontSize: '56px',
+                                fontWeight: 800,
+                                textTransform: 'uppercase',
+                                letterSpacing: '3px'
+                            }}
+                        >
+                            Welcome to Breeze!
+                        </Box>
+                        <Typography id="transition-modal-description" fontSize="1.0rem" sx={{ mt: 2 }}>
+                            Please feel free to interact with our product, view our <Link href="https://capstoneproject.app/" target="_blank">product portfolio</Link>, and check out our source code <Link href="https://github.com/todd-buck/CapstoneProject" target="_blank">GitHub</Link>.
+                        </Typography>
+                        <Box align="right" >
+                            <Button onClick={handleClose}>Ready to Go!</Button>
+                        </Box>
                     </Box>
-                </Box>
-                <BootstrapDialogTitle align="center" fontSize="4rem" id="customized-dialog-title" onClose={handleClose}>
-                    <Box
-                        sx={{
-                            color: 'black',
-                            fontSize: '56px',
-                            fontWeight: 800,
-                            textTransform: 'uppercase',
-                            letterSpacing: '3px'
-                        }}
-                    >
-                        Welcome to Breeze!
-                    </Box>
-
-                    
-                </BootstrapDialogTitle>
-                <DialogContent dividers>
-                    <Typography gutterBottom align="center">
-                        Please feel free to interact with our product, view our product portfolio, and check out our source code GitHub.
-                    </Typography>
-                    
-                </DialogContent>
-                <DialogActions>
-                    <Button autoFocus onClick={handleClose}>
-                        Ready to Go!  
-                    </Button>
-                </DialogActions>
-            </BootstrapDialog>
+                </Fade>
+            </Modal>
         </div>
     );
 }
